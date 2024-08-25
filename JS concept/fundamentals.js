a67b7b8776d7df9b96886e3172c72dd96df3f934
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-08-26 01:54:01 +0600

    fundamentals 6

---
+++ JS concept/fundamentals.js
@@ -0,0 +1,19 @@
+// 1. How to declare a variable using let and const 
+
+
+
+// 6 basic conditions >, <AbortController, ===, !==, =>, <=
+
+if(true){
+
+}else{
+
+}
+
+// 3. array 
+
+// 4. loop 
+
+// 5. function 
+
+// 6. object 
