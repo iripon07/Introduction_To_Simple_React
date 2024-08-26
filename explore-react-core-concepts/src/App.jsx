c18ed1295113d575f63360d46d37432fd707dba7
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-08-26 10:35:48 +0600

    using map

--- explore-react-core-concepts/src/App.jsx
+++ explore-react-core-concepts/src/App.jsx
@@ -1,13 +1,19 @@
+import Actor from "./Actor";
 import "./App.css";
 import Todo from "./Todo";
 
 function App() {
+
+  const actors =['Johny Depp', 'Decaprio', 'Christian Bale', 'Hug Jackman', 'Alfred Nobel']
   return (
     <>
       <h1>Vite + React</h1>
-      <Todo task="Learn React" isDone={true}></Todo>
+      {
+        actors.map(actor => <Actor name={actor} key={actor.indexOf}></Actor>)
+      }
+      {/* <Todo task="Learn React" isDone={true}></Todo>
       <Todo task="Learn JS" isDone={false}></Todo>
-      <Todo task="Learn Next JS" isDone={false}></Todo>
+      <Todo task="Learn Next JS" isDone={false}></Todo> */}
     </>
   );
 }
--- explore-react-core-concepts/src/Todo.jsx
+++ explore-react-core-concepts/src/Todo.jsx
@@ -6,6 +6,7 @@
 //   );
 // }
 
+
 export default function Todo({ task, isDone }) {
   if (isDone) return <li>Finished: {task}</li>;
   else return <li>Work on: {task}</li>;
