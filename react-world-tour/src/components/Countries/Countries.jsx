bdbb82de0e3a77b4489c58f41567f0848713c5e4
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-08-26 13:34:29 +0600

    countries and country

--- react-world-tour/src/components/Countries/Countries.jsx
+++ react-world-tour/src/components/Countries/Countries.jsx
@@ -1,9 +1,21 @@
-
+import { useEffect, useState } from "react";
+import Country from "../Country/Country";
 
 const Countries = () => {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch(`https://restcountries.com/v3.1/all`)
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
   return (
-    <div>Countries</div>
-  )
-}
+    <div>
+      <h1>Countries: {countries.length}</h1>
+      {
+        countries.map( country => <Country country={country} key={country.cca3}></Country>)
+      }
+    </div>
+  );
+};
 
-export default Countries
+export default Countries;
