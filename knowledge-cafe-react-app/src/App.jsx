ef846c9a08e1e5c482d72cf6dfdd9c77311b7b7d
AsadulIslamRipon <mdasadulripon@gmail.com>
2024-08-27 20:10:08 +0600

    create fake data of blogs and load in app

--- knowledge-cafe-react-app/src/App.jsx
+++ knowledge-cafe-react-app/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Blogs from "./components/Blogs/Blogs";
 
 function App() {
   return (
     <>
       <h1 className="text-6xl text-red-800">Knowledge Cafe</h1>
+      <Blogs></Blogs>
     </>
   );
 }
